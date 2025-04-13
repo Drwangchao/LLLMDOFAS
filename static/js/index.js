5e261ff886ab40eaaf2441418c15d39084ca4329
Drwangchao <35255235+Drwangchao@users.noreply.github.com>
2025-04-13 17:34:59 +0800

    Create index.js

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+	bulmaCarousel.attach('#results-carousel', {
+	  slidesToScroll: 1,
+	  slidesToShow: 1,
+	  autoplay: true,
+	  delay: 3000,
+	  loop: true,
+	  pauseOnHover: false,
+	  navigation: true,
+	  pagination: true
+	});
+  });
+  
